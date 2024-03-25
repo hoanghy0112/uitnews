6d6eaac064fdcaf2983c11bd116ba76a18f7796a
Zang <bt.hoanggiang@gmail.com>
2024-03-25 13:32:34 +0700

    update workflow file

--- apps/mobile/app/(app)/index.tsx
+++ apps/mobile/app/(app)/index.tsx
@@ -1,16 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Button from '../../src/components/Button/Button';
+import { View } from 'react-native';
 
 import { Text } from '@gluestack-ui/themed';
+import { useAuth } from '../../src/stores/auth.store';
+import { router } from 'expo-router';
+import { Button } from '../../src/components/ui/Button/Button';
 
 export default function Page() {
+    const { isLogin, authLogout } = useAuth();
+
+    if (!isLogin) {
+        router.replace('/login');
+    }
+
     return (
         <View>
             <StatusBar style="auto" />
             <Text>This is home page</Text>
-            <Button>Click me</Button>
+            <Button
+                onPress={() => {
+                    authLogout();
+                }}
+            >
+                <Text>Signout</Text>
+            </Button>
         </View>
     );
 }
